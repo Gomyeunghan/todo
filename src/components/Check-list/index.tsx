9f940ccad034f6003ba4359f8bf4efc2f7fa1b9f
Gomyeunghan <rhaudgks04@gmail.com>
2025-01-09 19:29:21 +0900

    초기세팅

---
+++ src/components/Check-list/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Icon from "../Icon/Icon";
+
+export default function CheckList() {
+  const [isActive, setActive] = useState(false);
+
+  const onClick = () => {
+    setActive(!isActive);
+  };
+
+  return !isActive ? (
+    <div
+      className="rounded-3xl border-solid border-2 border-slate-900 flex items-center gap-4 p-2"
+      onClick={onClick}
+    >
+      <Icon isActive={isActive} />
+      비타민 챙겨먹기
+    </div>
+  ) : (
+    <div>
+      <div
+        className="rounded-3xl border-solid border-2 border-slate-900 flex items-center gap-4 p-2 bg-violet-100"
+        onClick={onClick}
+      >
+        <Icon isActive={isActive} />
+        <span className="line-through">비타민 챙겨먹기</span>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,33 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        slate: {
+          100: "#F1F5F9",
+          200: "#E2E8F0",
+          300: "#CBD5E1",
+          400: "#94A3B8",
+          500: "#64748B",
+          800: "#1E293B",
+          900: "#0F172A",
+        },
+        primary: "#7C3AED",
+        point: "#F43F5E",
+        bgLight: "#EDE9FE",
+        yellowGreen: "#BEF264",
+        brown: "#92400E",
+        violet100: "#EDE9FE",
+      },
+      fontFamily: {
+        nanum: ["NanumSquare", "sans-serif"],
+      },
+      fontSize: {
+        20: ["20px", { lineHeight: "1.5" }],
+        18: ["18px", { lineHeight: "1.5" }],
+        16: ["16px", { lineHeight: "1.5" }],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
